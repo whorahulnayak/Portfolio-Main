b6b16fef46c99428800835ebb2d6972bdb79d733
Rahul Anil Nayak <whorahulnayak@gmail.com>
2022-03-31 14:35:52 +0530

    Update SocialMedia.jsx

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsTwitter, BsInstagram,BsLinkedin } from 'react-icons/bs';
+import { BsTwitter, BsInstagram,BsLinkedin,BsGithub } from 'react-icons/bs';
 import {Link} from 'react-router-dom';
 const SocialMedia = () => (
   <div className="app__social">
@@ -8,14 +8,25 @@ const SocialMedia = () => (
         <BsTwitter />
       </div>
     </Link>
-    <Link>
+
+    <Link to='https://www.linkedin.com/in/rahul-anil-nayak-039436166/'>
       <div>
         <BsLinkedin />
       </div>
     </Link>
-    <div>
-      <BsInstagram />
-    </div>
+
+    <Link to='https://github.com/whorahulnayak'>
+      <div>
+        <BsGithub />
+      </div>
+    </Link>
+
+    
+    <Link to='https://www.instagram.com/rahulxnayak/'>
+      <div>
+        <BsInstagram />
+      </div>
+    </Link>
   </div>
 );
 
